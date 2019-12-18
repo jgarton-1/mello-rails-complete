941b53ffeb058a96ab8361dc7cbfbd67426d81dc
Jonathan Garton <jon.garton94@gmail.com>
2019-12-18 21:52:38 +0000

    replaced jwt with token in home.js

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -13,7 +13,6 @@ function sendUserToBoards() {
   if (localStorage.getItem('user')) {
     location.replace('/boards');
   }
-}
 
 function setAuth(setting) {
   authSetting = setting;
@@ -64,10 +63,10 @@ function handleSignupResponse(status) {
 
 function handleLoginResponse(data, status, jqXHR) {
   if (status === 'success') {
-    let jwt = jqXHR.getResponseHeader('authorization');
+    let token = jqXHR.getResponseHeader('authorization');
     let user = JSON.stringify(data);
 
-    localStorage.setItem('authorization', jwt);
+    localStorage.setItem('authorization', token);
     localStorage.setItem('user', user);
     sendUserToBoards();
   } else {
